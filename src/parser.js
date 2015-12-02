f76fc4c34e693a52bb4156550fc1aaa4d9cb9482
Kevin Barabash <kevinb7@gmail.com>
2015-12-01 23:37:42 -0800

    ported the parser from the old mathipulator project

--- src/parser.js
+++ src/parser.js
@@ -1,129 +1,160 @@
-let index;
+const {
+    Equation,
+    Expression,
+    Product,
+    Operator,
+    Identifier,
+    Literal,
+    Power
+} = require('./ast.js');
 
-let parse = function(math) {
-    index = 0;
-    
-    let left = expression(math);
-    try {
-        match(math, ['=']);
-    } catch (e) {
-        return left;   
+function isAlpha(token) {
+    return (token >= 'a' && token <= 'z');
+}
+
+function isNumber(token) {
+    return /\d*\.\d+|\d+\.\d*|\d+/.test(token);
+}
+
+var tokenRegex = /([a-z])|([\(\)\+\-\/\*\^\=])|(\d*\.\d+|\d+\.\d*|\d+)/g;
+
+class Parser {
+
+    // TODO: switch from 'match' to 'exec' so that an invalid input raises an error
+    parse(input) {
+        this.i = 0;
+        this.tokens = input.match(tokenRegex);
+
+        return this.equation();
     }
-    let right = expression(math);
-    return { type: 'Equation', left, right };
-};
 
-let match = function(math, tokens) {
-    let token = math[index++];
-    if (!tokens.includes(token)) {
-        throw `expected one of ${tokens} got a ${token}`;
-    }
-    return token;
-};
+    equation() {
+        var lhs = this.expression();
+        var token = this.tokens[this.i++];
+        if (token === '=') {
+            var rhs = this.expression();
 
-let whitespace = function(math) {
-    while (math[index] === ' ') {
-        index++;
-    }
-};
-
-let expression = function(math) {
-    let left = number(math);
-    whitespace(math);
-    
-    while (index < math.length) {
-        try {
-            let operator = match(math, ['+', '-']);
-            let right = number(math);
-            left = {
-                type: 'BinaryExpression', left, right, operator
-            };
-            whitespace(math);
-        } catch (e) {
-            throw `expected one of ${['+', '-']} got a '${math[index]}'`;
+            return new Equation(lhs, rhs);
         }
+        return lhs;
+    };
+
+    expression() {
+        var tokens = this.tokens;
+        var children = [];
+
+        children.push(this.term(tokens));
+
+        var token = tokens[this.i++];
+        while (token === '+' || token === '-') {
+            children.push(new Operator(token));
+            children.push(this.term());
+            token = tokens[this.i++];
+        }
+        this.i--;
+
+        if (children.length === 1) {
+            return children[0];
+        }
+
+        return new Expression(...children);
     }
-    
-    return left;
-};
 
-let number = function(math) {
-    whitespace(math);
-    let naturalRegex = /[1-9]/;
-    let wholeRegex = /[0-9]/;
-    let decimalRegex = /[0-9\.]/;
-    let value;
+    term() {
+        var tokens = this.tokens;
+        var children = [];
 
-    if (math[index] === ".") {
-        let start = index++;
-        while (wholeRegex.test(math[index])) {
-            index++;
-        }
-        value = math.substring(start, index);
-    } else if (math[index] === "0") {
-        let start = index++;
-        match(math, ["."]);
-        while (wholeRegex.test(math[index])) {
-            index++;
-        }
-        value = math.substring(start, index);
-    } else if (naturalRegex.test(math[index])) {
-        let start = index++;
-        let regex = decimalRegex;
-        while (regex.test(math[index])) {
-            if (math[index] === ".") {
-                regex = wholeRegex;
+        children.push(this.factor(tokens));
+
+        var token = tokens[this.i++];
+
+        // TODO: handle division separately
+        while (token === '*' || token === '(' || isAlpha(token)) {
+            if (token === '(') {
+
+                children.push(new Operator('*'));
+
+                var expr = this.expression();
+                token = tokens[this.i++];
+                if (token !== ')') {
+                    throw 'expected )';
+                }
+                children.push(expr);
+
+            } else if (isAlpha(token)) {  // TODO: figure out why we can't let factor() handle this
+                children.push(new Operator('*'));
+                this.i--; // put the alpha back on so factor() can deal with it
+                // TODO: create a peek function to handle this more elegantly
+                children.push(this.factor());
+                token = tokens[this.i++];
+            } else {
+                children.push(new Operator('*'));
+                children.push(this.factor());
+                token = tokens[this.i++];
             }
-            index++;
+
+            if (this.i > tokens.length) {
+                break;
+            }
         }
-        value = math.substring(start, index);
-    } else {
-        throw `expected a number`;
+        this.i--;
+
+        if (children.length === 1) {
+            return children[0];
+        }
+
+        return new Product(...children);
+    };
+
+    factor() {
+        var tokens = this.tokens;
+        var token = tokens[this.i++];
+        var sign = '';
+
+        // TODO: think about multiple unary minuses
+        if (token === '+' || token === '-') {
+            sign = token;
+            token = tokens[this.i++];
+        }
+
+
+        var base, exp;
+
+        if (isAlpha(token)) {
+            base = new Identifier(token);
+        } else if (isNumber(token)) {
+            base = new Literal(parseFloat(token));
+        } else if (token === '(') {
+            base = this.expression();
+            token = tokens[this.i++];
+            if (token !== ')') {
+                throw 'expected )';
+            }
+        }
+
+        if (tokens[this.i++] === '^') {
+            exp = this.factor();
+            return new Power(base, exp);
+        } else {
+            this.i--;
+
+            var factor = base;
+
+            // TODO: handle unary minus
+            //if ($(factor).is('mrow')) {
+            //    if (sign !== '') {
+            //        $(factor).attr('unary', 'minus'); // TODO: need to update evaluate.js to understand this
+            //    }
+            //} else {
+            //    var text = $(factor).text();
+            //    $(factor).text(sign + text);
+            //}
+
+            return factor;
+        }
     }
-    
-    // We store the value as a string so that we can maintain its formatting.
-    // Only when we evaluate it do we convert it to a float.
-    return { type: 'Number', value };
-};
 
+}
 
+module.exports = Parser;
 
-let render = function(node) {
-    if (node.type === 'Number') {
-        return node.value;
-    } else if (node.type === 'BinaryExpression') {
-        let left = render(node.left);
-        let right = render(node.right);
-        return `${left} ${node.operator} ${right}`;
-    }
-};
-
-let renderHTML = function(node) {
-    if (node.type === 'Number') {
-        return `<span>${node.value}</span>`;
-    } else if (node.type === 'BinaryExpression') {
-        let left = renderHTML(node.left);
-        let right = renderHTML(node.right);
-        return `${left} ${node.operator} ${right}`;
-    }
-};
-
-let evaluate = function(ast, context) {
-    
-};
-
-/**
- * Return a new AST where identifiers with keys in context have been replaced
- * with the corresponding value which must be an AST node.
- * @param ast
- * @param context
- */
-let substitute = function(ast, context) {
-    
-};
-
-module.exports = {
-    parse: parse,
-    render: render,
-    renderHTML: renderHTML
-};
