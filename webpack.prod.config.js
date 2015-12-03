b84e30a6263477d5089b11bebf2efda8bdec5f65
Kevin Barabash <kevinb7@gmail.com>
2015-12-02 21:15:44 -0800

    add webpack config for production builds

---
+++ webpack.prod.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            include: path.join(__dirname, 'src'),
+            query: {
+                optional: ['es7.classProperties']
+            }
+        }, {
+            test: /\.json$/,
+            loader: 'json',
+            include: path.join(__dirname, 'metrics')
+        }]
+    }
+};
