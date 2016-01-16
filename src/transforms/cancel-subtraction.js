5382ffe8c2a61152a76bbf8ce9a35ec86b8a0707
Kevin Barabash <kevinb7@gmail.com>
2016-01-16 11:17:49 -0800

    update cancel-subtraction to require selecting the operands

--- src/transforms/cancel-subtraction.js
+++ src/transforms/cancel-subtraction.js
@@ -2,23 +2,34 @@ const { Literal } = require('../ast.js');
 const { deepEqual } = require('../util/node_utils.js');
 
 function canTransform(selection) {
-    if (selection.type === 'range') {
-        return false;
+    if (selection.length === 1 && ['Expression', 'Product'].includes(selection.first.type)) {
+        selection = selection.first;
     }
-    const node = selection.first;
-    if (node.type === 'Operator' && node.operator === '-') {
-        return deepEqual(node.prev, node.next);
+    if (selection.length === 3) {
+        const [first, operator, last] = selection;
+
+        if (operator.operator === '-') {
+            if (first.prev && first.prev.operator === '-') {
+                return false;
+            } else {
+                return deepEqual(first, last);
+            }
+        }
     }
     return false;
 }
 
 function doTransform(selection) {
     if (canTransform(selection)) {
-        const node = selection.first;
-        const { parent, prev, next } = node;
-        parent.remove(prev);
-        parent.remove(next);
-        parent.replace(node, new Literal(0));
+        if (selection.length === 1 && ['Expression', 'Product'].includes(selection.first.type)) {
+            selection = selection.first;
+        }
+        const [first, operator, last] = selection;
+        const parent = operator.parent;
+
+        parent.remove(first);
+        parent.remove(last);
+        parent.replace(operator, new Literal(0));
     }
 }
 
